063cee763956752fa5cb26cadcab1263ff528fe6
garold664 <garold664@gmail.com>
2024-08-16 20:09:05 +0300

    creates primary color in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: { ...colors.cyan, DEFAULT: colors.cyan[500] },
+      },
+    },
   },
   plugins: [],
 };
