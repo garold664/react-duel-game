853088af7ad8821e0e1009b8e67fc5d5b22eb9c6
garold664 <garold664@gmail.com>
2024-08-16 20:01:13 +0300

    creates canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { useRef } from 'react';
 import './App.css';
 
 function App() {
-  return <></>;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  return (
+    <>
+      <canvas width={800} height={800} ref={canvasRef}></canvas>
+    </>
+  );
 }
 
 export default App;
