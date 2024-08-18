2db6667ab3a5fe4ffc80f6433b123f738439c124
garold664 <garold664@gmail.com>
2024-08-18 08:26:59 +0300

    implement bouncing of the cursor fully

--- src/helpers/movePlayer.ts
+++ src/helpers/movePlayer.ts
@@ -2,6 +2,7 @@ import { Figure } from './classes';
 import { CANVAS_HEIGHT, PLAYER_RADIUS } from './constants';
 
 function movePlayer(player: Figure, mouseCoords: { x: number; y: number }) {
+  const OVERLAP = 5;
   if (
     player.y > CANVAS_HEIGHT - PLAYER_RADIUS ||
     player.y < 0 + PLAYER_RADIUS
@@ -9,27 +10,24 @@ function movePlayer(player: Figure, mouseCoords: { x: number; y: number }) {
     player.speed = player.speed * -1;
   }
 
-  // if (
-  //   player.speed > 0 &&
-  //   player.y + PLAYER_RADIUS > mouseCoords.y &&
-  //   player.x + PLAYER_RADIUS > mouseCoords.x &&
-  //   player.x - PLAYER_RADIUS < mouseCoords.x
-  // ) {
-  //   player.speed = -player.speed;
-  //   console.log(player.speed);
-  //   console.log('cursor touch');
-  // }
-
   if (
-    player.speed < 0 &&
-    player.y - PLAYER_RADIUS < mouseCoords.y &&
-    player.y - PLAYER_RADIUS + 3 > mouseCoords.y &&
+    player.speed > 0 &&
+    player.y + PLAYER_RADIUS > mouseCoords.y &&
+    player.y + PLAYER_RADIUS - OVERLAP < mouseCoords.y &&
     player.x + PLAYER_RADIUS > mouseCoords.x &&
     player.x - PLAYER_RADIUS < mouseCoords.x
   ) {
     player.speed = -player.speed;
-    console.log(player.speed);
-    console.log('cursor touch');
+  }
+
+  if (
+    player.speed < 0 &&
+    player.y - PLAYER_RADIUS < mouseCoords.y &&
+    player.y - PLAYER_RADIUS + OVERLAP > mouseCoords.y &&
+    player.x + PLAYER_RADIUS > mouseCoords.x &&
+    player.x - PLAYER_RADIUS < mouseCoords.x
+  ) {
+    player.speed = -player.speed;
   }
 
   player.y += player.speed;
