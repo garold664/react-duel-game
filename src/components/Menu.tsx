e4537f702cfbc0a665da735ebefed5ba24c2f9df
garold664 <garold664@gmail.com>
2024-08-19 13:09:50 +0300

    adjust the styles of the Menu component

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -12,17 +12,27 @@ export default function Menu({ setIsMenuShown, currentPlayer }: MenuProps) {
   const [shootRate, setShootRate] = useState(
     Math.abs(currentPlayer.shootingRate)
   );
+  const [bulletColor, setBulletColor] = useState(currentPlayer.bulletColor);
 
   return (
     <div className="absolute inset-1/2 -translate-x-1/2 -translate-y-1/2 w-96 h-96 backdrop-blur-md border-primary-500 border-4 rounded-xl flex  flex-col justify-center items-center bg-primary-900/35 shadow-lg shadow-primary-900">
-      <h2>{`Menu for ${currentPlayer.name}`}</h2>
-      <input
-        type="color"
-        value={currentPlayer.bulletColor}
-        onChange={(e) => {
-          currentPlayer.bulletColor = e.target.value;
-        }}
-      />
+      <h2>{`Set color of bullets for ${currentPlayer.name}`}</h2>
+
+      <label
+        className="w-32 h-32 rounded-full flex justify-center items-center"
+        style={{ backgroundColor: bulletColor }}
+      >
+        <input
+          className="opacity-0 w-0 h-0"
+          type="color"
+          value={currentPlayer.bulletColor}
+          onChange={(e) => {
+            currentPlayer.bulletColor = e.target.value;
+            setBulletColor(e.target.value);
+          }}
+        />
+      </label>
+
       <Range
         min={0}
         max={5}
@@ -43,7 +53,12 @@ export default function Menu({ setIsMenuShown, currentPlayer }: MenuProps) {
         }}
       />
 
-      <button onClick={() => setIsMenuShown(false)}>X</button>
+      <button
+        className="absolute right-2 top-2 p-0 w-10 h-10 rounded-full"
+        onClick={() => setIsMenuShown(false)}
+      >
+        X
+      </button>
     </div>
   );
 }
