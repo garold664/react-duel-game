01f3a2cb5a2dbd19049a88a695a2941bd975733c
garold664 <garold664@gmail.com>
2024-08-20 15:56:50 +0300

    fix Menu's z-index

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -10,7 +10,7 @@ export default function Menu({ setIsMenuShown, currentPlayer }: MenuProps) {
   const [bulletColor, setBulletColor] = useState(currentPlayer.bulletColor);
 
   return (
-    <div className="absolute inset-1/2 -translate-x-1/2 -translate-y-1/2 w-96 h-96 backdrop-blur-md border-primary-500 border-4 rounded-xl flex  flex-col justify-center items-center bg-primary-900/35 shadow-lg shadow-primary-900">
+    <div className="absolute inset-1/2 -translate-x-1/2 -translate-y-1/2 w-96 h-96 backdrop-blur-md border-primary-500 border-4 rounded-xl flex  flex-col justify-center items-center bg-primary-900/35 shadow-lg shadow-primary-900 z-[100]">
       <h2>{`Set color of bullets for ${currentPlayer.name}`}</h2>
 
       <label
